26a5ee136717aefcf5b52d1cb0489af23efa52d6
BielKaiser <97536229+BielKaiser@users.noreply.github.com>
2023-03-02 17:11:58 -0300

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,67 @@
+$(document).ready(function(){
+
+    console.log('Ready')
+
+    //  Busque a data atual e atualize-a no DOM
+var data = new Date()
+var arrumar = data.toLocaleDateString()
+$('#date').text(arrumar)
+
+
+
+
+    // Escreva um evento, quando o botão Enviar for clicado
+    $('#button').click(function(){
+
+        //  Obtenha o valor do texto da área de texto usando o método 'val()'
+        let text_value = $('#text').val()
+
+        //  Converta-o em um objeto JS
+        //  Forneça uma "chave" aqui e escreva o mesmo no arquivo app.py também para extrair dados
+        let input_text = {'chave' : text_value}
+        console.log(input_text)
+
+        //  requisição ajax
+        $.ajax({
+            url:'/prever',
+            //  tipo da requisição web
+            type : 'POST',
+
+            //  dados a serem enviados no formato JSON
+            data : JSON.stringify(input_text),
+
+            //  o tipo de resposta esperado é json
+            dataType : 'json',
+
+            //  contentType
+            contentType : 'application/json',
+
+            //  se tudo funcionar, execute esta função
+            success : function(result){
+
+                // extraia previsão e a URL do emoticon do resultado
+                extract = result.prediction
+                emoji = result.url
+                //  atualize os elementos DOM
+                $('#sentiment').text(extract)
+                $('#emoji').attr('src',emoji)
+                //  exiba-os
+                $('#sentiment').show()
+
+                $('#emoji').show()
+
+            },
+
+            //  se houver algum erro, execute esta função
+            error : function(result){
+
+                console.log(result)
+            }
+        })
+
+
+        //  limpando a caixa de texto após cada pressionamento de botão
+        $('#text').val("")
+    })
+        
+})
